5cb19d07538f6cce97993b80196e1871e11f15f5
pywkt <prvd@pywkt.com>
2022-10-20 22:39:33 +0000

    attempt 01 at getting time and notes to work together

--- src/App.js
+++ src/App.js
@@ -1,114 +1,88 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Chord, Key, Note, Progression, Scale } from '@tonaljs/tonal';
+import * as Tone from 'tone';
 import './App.css';
 import Button from './components/Button'
-import { getRand } from './util';
+import { allNotes, buildLoop, getRand, processMajor } from './util';
 
 function App() {
-  // console.log("C#:", Note.get("C#"))
-  // console.log('Db:', Note.get("Db"))
-
-  const allNotes = ["C", "D", "E", "F", "G", "A", "B", "Cb", "Db", "Eb", "Fb", "Gb", "Ab", "Bb", "C#", "D#", "E#", "F#", "G#", "A#", "B#"]
-
-  // const getKey = (tonic, scale) => {
-  //   const res = scale === "major" ? Key.majorKey(tonic) : Key.minorKey(tonic)
-  //   console.log("getKey res:", res)
-  //   const randomKeyScale = Scale.get(res.chordScales[getRand(0, res.chordScales.length)])
-  //   console.log('Key:', res)
-  //   console.log('randomKeyScale:', randomKeyScale)
-
-  //   const combinedChords = [...res.chords, ...randomKeyScale.notes]
-  //   console.log('combinedChords:', combinedChords)
-  //   const chordRes = getChord(combinedChords[getRand(0, combinedChords.length)])
-
-  //   // const chordRes = getChord(randomKeyScale.notes[getRand(0, randomKeyScale.notes.length)])
-  //   console.log('chordRes:', chordRes)
-
-  //   const allChordArray = [];
-  //   combinedChords.forEach(c => {
-  //     const currentChord = Chord.get(c)
-  //     allChordArray.push({ name: currentChord.symbol, notes: currentChord.notes })
-  //   })
-
-  //   console.log('allChordArray:', allChordArray)
-  // }
-
-  // const getChord = (chord) => {
-  //   console.log('chord:', chord)
-  //   const res = Chord.get(chord)
-  //   // console.log('chord res:', res)
-  //   return res
-  // }
-
-  // const getScales = () => {
-  //   const allScales = Scale.names();
-  //   const randomScale = allScales[getRand(0, allScales.length)];
-  //   const selectedScaleData = Scale.get(randomScale);
-  //   const scaleChords = Scale.scaleChords(randomScale);
-  //   const scaleReduced = Scale.reduced(randomScale.name)
-  //   // const scaleNotes = Scale.scaleNotes(randomScale)
-
-  //   console.log("selectedScaleData:", selectedScaleData)
-  //   console.log('scaleChords:', scaleChords)
-  //   console.log('scaleReduced:', scaleReduced)
-
-  // }
-
-  const getNotes = () => {
-    console.log('ee')
-  }
+  const [stateChords, setStateChords] = useState({})
+  console.log('state:', stateChords)
 
   const processMinor = (data) => {
     console.log('processMinor data:', data)
   }
 
-  const getProgression = () => {
+  // const getKeyData = (tonic, scale) => scale === "major" ? setChords(Key.majorKey(tonic)) : processMinor(Key.minorKey(tonic))
+  
+  const setChords = (data) => setStateChords(processMajor(data))
 
-  }
+  const handleBuildLoop = (data) => setStateChords(buildLoop(data))
+  const getKeyData = (tonic, scale) => scale === "major" ? handleBuildLoop(Key.majorKey(tonic)) : processMinor(Key.minorKey(tonic))
+ 
 
-  // Backup
-  // const processMajor = (data) => {
-  //   console.log('processMajor data:', data)
-  //   const { chords } = data
 
-  //   const selectedScale = [];
+  // const noteLens = ["1n", "2n", "4n", "8n"]
 
-  //   chords.forEach(chord => {
-  //     const currentChord = Chord.get(chord)
-  //     // console.log('currentChord:', currentChord)
-  //     selectedScale.push({ chord: currentChord.notes, name: currentChord.name, symbol: currentChord.symbol})
-  //   })
 
-  //   console.log('selectedScale:', selectedScale)
-  // }
+  // console.log(Progression.fromRomanNumerals("C", progs[0]))
 
-  const processMajor = (data) => {
-    console.log('processMajor data:', data)
-    const { chords, chordScales } = data
-    const randScale = chordScales[getRand(0, chordScales.length)]
-    console.log('randScale:', randScale)
-    // const randScaleChords = Scale.scaleChords(randScale)
-    const randScaleData = {...Scale.get(randScale), chords: Scale.scaleChords(randScale)}
-    console.log('randScaleData:', randScaleData)
+  const playNotes = () => {
+    Tone.Transport.bpm.value = 120;
+    const now = Tone.now();
 
-    const chordsToUse = [];
-    randScaleData.notes.forEach(note => {
-      const currentChord = Chord.getChord(randScaleData.chords[getRand(0, randScaleData.chords.length)], randScaleData.notes[getRand(0, randScaleData.notes.length)])
-      chordsToUse.push({ notes: currentChord.notes, symbol: currentChord.symbol, name: currentChord.name})
+    const synth = new Tone.PolySynth(Tone.FMSynth).toDestination()
+
+    // const chordToPlay = stateChords[getRand(0, Object.keys(stateChords).length)].notes
+    // const withOct = chordToPlay.map((c, i) => c = c + (i === chordToPlay.length - 1 ? 4 : 3))
+
+    
+
+    // const slimOct = [];
+    // // if (withOct.length > 3) {
+    //   const amountToGet = 3;
+    //   for (let i = 1; i <= amountToGet; i += 1) {
+    //     slimOct.push(withOct[getRand(0, withOct.length)])
+    //   }
+    // // }
+
+    // console.log('slimOct:', slimOct)
+
+    // stateChords.map((s, i) => {
+    //   // console.log('s.notes:', s.notes + 3)
+    //   synth.triggerAttackRelease(s.notes + 3, "2n", now)
+    // })
+
+    // const makeBars = () => {}
+
+    // makeLoop({ notes: withOct, len: "2n"})
+
+    // console.log('chordToPlay:', chordToPlay)
+    // console.log('withOct:', withOct)
+    
+    stateChords.map(c => {
+      synth.triggerAttackRelease(c.notes, c.noteData.name, c.timeBar)
     })
+    
 
-    console.log('chordsToUse:', chordsToUse)    
+    // const loop01 = new Tone.Loop((time) => {
 
+    //   synth.triggerAttackRelease(slimOct, "4n")
+    // }).start("0:0:0").stop('1:0:0')
+
+    Tone.Transport.start()
+    // introLoop()
+    // synth.triggerAttackRelease(withOct[1], "2n", now + 1)
+    // synth.triggerAttackRelease(withOct[2], "2n", now + 2)
+    // synth.triggerAttackRelease(withOct[3], "2n", now + 3)
   }
-
-  const getKeyData = (tonic, scale) => scale === "major" ? processMajor(Key.majorKey(tonic)) : processMinor(Key.minorKey(tonic))
 
   return (
     <div className="App">
       <Button onClick={() => getKeyData(allNotes[getRand(0, allNotes.length)], "major")} label="Get Data" />
-      {/* <Button onClick={getScales} label="Get Scales" />
-      <Button onClick={getNotes} label="Get Notes" />
-      <Button onClick={() => getKey(allNotes[getRand(0, allNotes.length)], "major")} label="Get Key" /> */}
+      <Button onClick={playNotes} label="Play Notes" />
+      <br />
+      <Button onClick={() => getKeyData(allNotes[getRand(0, allNotes.length)], "major")} label="buildLoop" />
     </div>
   );
 }
--- src/util/index.js
+++ src/util/index.js
@@ -1,1 +1,262 @@
+import { Chord, Note, Progression, Scale } from '@tonaljs/tonal';
+
+export const allNotes = ["C", "D", "E", "F", "G", "A", "B", "Cb", "Db", "Eb", "Fb", "Gb", "Ab", "Bb", "C#", "D#", "E#", "F#", "G#", "A#", "B#"]
+
 export const getRand = (min, max) => Math.floor(Math.random() * (max - min) + min);
+
+export const getProgression = (tonic) => {
+    // console.log('tonic:', Chord.get(tonic))
+    const progs = [["I", "V", "vi", "IV"], ["I", "IV", "V", "I"]];
+
+    const prog = Progression.fromRomanNumerals(tonic, progs[getRand(0, progs.length)]);
+    // console.log('prog in func:', prog)
+
+    const progArr = [];
+    prog.forEach(p => {
+        const currentNote = Chord.get(p)
+        const cleanNotes = currentNote.notes.map(n => n = Note.simplify(n))
+        // console.log('clean:', cleanNotes)
+        progArr.push({ notes: cleanNotes, symbol: currentNote.symbol, name: currentNote.name })
+    })
+    // console.log('prog02:', progArr)
+    return progArr
+
+}
+
+export const processMajor = (data, useProg) => {
+    // console.log('processMajor data:', data)
+    const { chordScales, tonic } = data
+    const randScale = chordScales[getRand(0, chordScales.length)]
+    const randScaleData = { ...Scale.get(randScale), chords: Scale.scaleChords(randScale) }
+
+    const chordsToUse = [];
+    randScaleData.notes.forEach(note => {
+        const currentChord = Chord.getChord(randScaleData.chords[getRand(0, randScaleData.chords.length)], randScaleData.notes[getRand(0, randScaleData.notes.length)])
+
+        currentChord.notes.forEach(cc => {
+            if (Note.simplify(cc) !== "") {
+                cc = Note.simplify(cc)
+            }
+        })
+
+        chordsToUse.push({ notes: currentChord.notes, symbol: currentChord.symbol, name: currentChord.name })
+    })
+
+    // console.log('chordsToUse:', chordsToUse)
+
+    const progOut = getProgression(tonic)
+    // console.log("progOut:", progOut)
+
+    return useProg ? progOut : chordsToUse
+
+}
+
+const addOct = (data) => {
+    const slimOct = [];
+
+    data.map(d => {
+        slimOct.push({
+            ...d, notes: d.notes.map(n => {
+                if (/\#\#/.test(n)) {
+                    return n = Note.simplify(n) + 4
+                }
+
+                return n = n + 3
+            })
+        })
+    })
+
+    return slimOct;
+}
+
+const makeBars = () => {
+    const measure = getRand(0, 4)
+    const quarter = getRand(0, 4)
+    const sixteenth = getRand(0, 16)
+    // return `${measure}:${quarter}:${sixteenth}`
+    return `${measure}:${quarter}:0`
+}
+
+const makeNoteLen = () => {
+    // const noteLens = ["1n", "2n", "4n", "8n"]
+    const noteLens = [
+        { name: "1n", value: 1 },
+        { name: "2n", value: 0.5 },
+        { name: "4n", value: 0.25 },
+        { name: "8n", value: 0.08 }
+    ]
+    return noteLens[getRand(0, noteLens.length)]
+}
+
+const generateNoteLen = (data) => {
+    // console.log('generateTime data:', data)
+    const withNoteLen = [];
+
+    data.map(d => {
+        withNoteLen.push({
+            ...d,
+            // timeBars: makeBars(),
+            noteLen: makeNoteLen()
+        })
+
+    })
+
+    return withNoteLen
+}
+
+// const trimDupes = (data) => {
+//     console.log('trimDupes:', data)
+//     const trimmedDupes = [];
+
+//     data.map((d, i, a) => {
+//         const dReg = d.noteLen.match(/(\d+)([a-z])/)
+//         // const timeSplit = d.timeBars.split(':')
+//         // console.log('timesplit:', timeSplit)
+//         console.log('dReg:', dReg)
+//         // if (dReg[1])
+//     })
+
+//     return trimmedDupes;
+
+// }
+
+const generateTimeBars = (data) => {
+    const withTimeBars = [];
+    const noteReg = /(\d+)([a-z])/
+    let mLen = 0
+
+    data.map((d, i, a) => {
+        const noteNumber = d.noteLen.match(/(\d+)([a-z])/)[1]
+        console.log(noteNumber)
+
+        let validTimeBar;
+        if (i === 0) {
+            validTimeBar = "0:0:0"
+            console.log('one')
+        }
+        else {
+            let qLen = (withTimeBars[i - 1].noteLen).match(/\d+/)[0]
+            let previousBeat = String((withTimeBars[i - 1].timeBar)).split(':')[1]
+
+            qLen = Number(qLen) + Number(previousBeat)
+
+            if ((previousBeat + qLen) > 4) {
+                qLen = qLen % 4
+                mLen = mLen + 1
+            }
+
+            validTimeBar = `${mLen}:${qLen}:0`
+        }
+
+
+
+        withTimeBars.push({
+            ...d,
+            timeBar: validTimeBar
+        })
+    })
+
+    return withTimeBars
+}
+
+const noteLens = [
+    { name: "1n", value: 1, num: 1, mod: 'n' },
+    { name: "2n", value: 0.5, num: 2, mod: 'n' },
+    { name: "4n", value: 0.25, num: 4, mod: 'n' },
+    { name: "8n", value: 0.08, num: 8, mod: 'n' }
+]
+
+const generateNoteLen02 = (data) => {
+    // console.log('data:', data)
+    const withNoteData = [];
+
+    data.map((d, i, a) => {
+        withNoteData.push({
+            ...d,
+            noteData: noteLens[getRand(0, noteLens.length)]
+        })
+    })
+
+    return withNoteData;
+
+}
+
+const generateTimeBars02 = (data) => {
+    console.log('data:', data)
+    const withTimeBars = [];
+    let validTimeBar;
+    let mTime = 0;
+    let qTime = 0;
+    let sTime = 0;
+
+    data.map((d, i, a) => {
+        const { name, num, value } = d.noteData
+
+        if (i === 0) {
+            validTimeBar = "0:0:0"
+        } else {
+            let previousQn = withTimeBars[i - 1].noteData.num
+            let previousTime = withTimeBars[i - 1].timeBar
+            let previousQTime = previousTime.split(':')[1]
+            let previousMTime = previousTime.split(':')[0]
+
+            if (previousQn === 8) {
+                sTime = sTime + 8
+            }
+
+            if (previousQn === 4) {
+                qTime = qTime + 1
+            }
+
+            if (previousQn === 2) {
+                qTime = qTime + 2
+            }
+
+            if (previousQn === 1) {
+                qTime = qTime + 4
+            }
+
+            if (sTime > 16) {
+                sTime = sTime % 16
+                qTime = qTime + 1
+            }
+
+            if (qTime > 4) {
+                qTime = qTime % 4
+                mTime = mTime + 1
+            }
+
+            
+
+            // let qTime = Number(previousQTime) + previousQn
+
+            // if (qTime > 4) {
+            //     qTime = qTime % 4
+            //     mTime = mTime + 1
+            // }
+
+            validTimeBar = `${mTime}:${qTime}:${sTime}`
+        }
+
+        withTimeBars.push({ ...d, timeBar: validTimeBar })
+
+    })
+
+    return withTimeBars;
+}
+
+export const buildLoop = (data) => {
+    const chordObj = processMajor(data)
+    const withOct = addOct(chordObj)
+
+    const withNoteLen = generateNoteLen02(withOct)
+    // console.log('withNoteLen:', withNoteLen)
+
+    const withTimeBars = generateTimeBars02(withNoteLen)
+    console.log('withTimeBars:', withTimeBars)
+
+    return withTimeBars
+
+
+
+}
