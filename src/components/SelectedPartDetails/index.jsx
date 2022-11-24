52610276ed8f3e6b64691f1707043b8bd1573735
pywkt <gitlab@pywkt.com>
2022-11-24 06:21:33 +0000

    added button to hide piano

--- src/components/SelectedPartDetails/index.jsx
+++ src/components/SelectedPartDetails/index.jsx
@@ -1,74 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Chord, Note } from '@tonaljs/tonal';
 import styles from '../../styles/SelectedPartDetails.module.scss';
 
 const SelectedPartDetails = ({ currentNote, instrumentName }) => {
-    // if (!currentNote) {
-    //     return null
-    // }
+    const [isVisible, setIsVisible] = useState(true);
+    console.log(currentNote)
 
     const allNotes = Array.apply(null, { length: 12 })
     const halfNoteIndexes = [1, 3, 6, 8, 10, 13, 15, 18, 20, 22]
 
     let noteIndex = currentNote?.map(Note.chroma)
 
+    const togglePiano = () => setIsVisible(prev => !prev)
 
     return (
         <div>
+            {isVisible &&
+            <>
+                <div className={styles.noteList}>
+                    {currentNote && (
+                        <>
+                            <div>
+                                {currentNote?.map(n => (
+                                    <div key={n}>{Note.simplify(n)}</div>
+                                ))}
+                            </div>
 
-            
+                            <div className={styles.chordName}>
+                                ({Chord.detect(currentNote)})
+                            </div>
+                        </>
+                    )}
+                </div>
 
-            <div className={styles.noteList}>
-                {currentNote && (
-                    <>
-                        <div>
-                            {currentNote?.map(n => (
-                                <div key={n}>{Note.simplify(n)}</div>
-                            ))}
-                        </div>
+                <div className={styles.partDetailsText}>{instrumentName}</div>
+                <div className={styles.notesContainer}>
 
-                        
-
-
-
-                        <div className={styles.chordName}>
-                            ({Chord.detect(currentNote)})
-                        </div>
-                    </>
-                )}
-            </div>
-
-            <div className={styles.partDetailsText}>{instrumentName}</div>
-            <div className={styles.notesContainer}>
-
-                {allNotes.map((note, index) => {
-                    if (halfNoteIndexes?.includes(index)) {
-                        return (
-                            <div
-                                key={`n${index}`}
-                                id={styles[`n${index}`]}
-                                className={`
+                    {allNotes.map((note, index) => {
+                        if (halfNoteIndexes?.includes(index)) {
+                            return (
+                                <div
+                                    key={`n${index}`}
+                                    id={styles[`n${index}`]}
+                                    className={`
                                 ${styles.allNotes} 
                                 ${styles.halfNote} 
                                 ${noteIndex?.includes(index) ? styles.filled : ""}`}
-                            />
+                                />
 
-                        )
-                    } else {
-                        return (
-                            <div
-                                key={`n${index}`}
-                                id={styles[`n${index}`]}
-                                className={`
+                            )
+                        } else {
+                            return (
+                                <div
+                                    key={`n${index}`}
+                                    id={styles[`n${index}`]}
+                                    className={`
                                 ${styles.allNotes} 
                                 ${styles.wholeNote}
                                 ${currentNote?.map(Note.chroma)?.includes(index) ? styles.filled : ""}
                                 `}
-                            />
-                        )
-                    }
-                })}
+                                />
+                            )
+                        }
+                    })}
+                </div>
+            </>
+            }
+           
+            
+            <div className={styles.hidePianoButton}>
+                <button type="button" onClick={togglePiano}>{isVisible ? "Hide Piano" : "Show Piano"}</button>
             </div>
+
         </div>
     )
 }
